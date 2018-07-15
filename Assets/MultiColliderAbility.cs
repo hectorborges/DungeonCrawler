14a879709462313a6253eeb94e40580fda1e8b12
hectorborges <hectorborges94@gmail.com>
2018-07-15 14:04:58 -0500

    added elemental air

---
+++ Assets/MultiColliderAbility.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiColliderAbility : Ability
+{
+    public int damage;
+    public Collider[] damageColliders;
+    public float activationFrequency;
+    public float extraEffectWaitTime;
+    public GameObject abilityEffect;
+
+    public override void ActivateAbility()
+    {
+        StartCoroutine(ActivationFrequency());
+    }
+
+    IEnumerator ActivationFrequency()
+    {
+        abilityEffect.SetActive(true);
+        for (int i = 0; i < damageColliders.Length; i++)
+        {
+            damageColliders[i].enabled = true;
+            yield return new WaitForSeconds(activationFrequency);
+            damageColliders[i].enabled = false;
+        }
+        yield return new WaitForSeconds(extraEffectWaitTime);
+        abilityEffect.SetActive(false);
+    }
+}
