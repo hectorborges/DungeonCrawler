ff0415e09f570950afeb15adf2693618114a6153
hectorborges <hectorborges94@gmail.com>
2018-07-15 00:58:08 -0500

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 4.0f;
+    public Vector3 cameraOffset;
+
+    private Vector3 offset;
+    Transform player;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        player = GameObject.Find("Player").transform;
+        offset = new Vector3(player.position.x, player.position.y + cameraOffset.y, player.position.z + cameraOffset.z);
+    }
+
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    public float runSpeed;
+    public float sprintSpeed;
+
+    [Space]
+    public float runToIdleSpeed;
+    public float idleToRunSpeed;
+    public float runToSprintSpeed;
+    public float sprintToRunSpeed;
+
+    [Space]
+    public float rotationSpeed;
+
+    [Space, Header("Jump Variables")]
+    public float jumpForce;
+    public float distanceToGround;
+    public LayerMask groundLayer;
+
+    [Space, Header("Crouch Variables")]
+    public float crouchSpeed;
+    public float resizeSpeed;
+    public CapsuleCollider crouchCollider;
+
+    [Space, Header("Roll Variables")]
+    public float rollForce;
+    public float rollLength;
+
+    //Input
+    float moveX;
+    float moveY;
+
+    bool sprint;
+    bool jump;
+    bool crouch;
+    bool roll;
+
+    //Gameplay
+    Vector3 movement;
+    Vector3 airMovement;
+    Quaternion direction;
+    Quaternion rollDirection;
+
+    float speed;
+    float jumpSpeed;
+
+    float baseRunSpeed;
+    float baseSprintSpeed;
+
+    float colliderHeight;
+
+    float rollStartTime;
+
+    bool isJumping;
+    bool isCrouching;
+    bool isRolling;
+
+    Animator animator;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+
+        colliderHeight = crouchCollider.height;
+        baseRunSpeed = runSpeed;
+        baseSprintSpeed = sprintSpeed;
+    }
+
+    private void Update()
+    {
+        RecieveInput();
+        Move();
+        Jump();
+        Crouch();
+        Roll();
+    }
+
+    void RecieveInput()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        sprint = Input.GetKey(KeyCode.LeftShift);
+        jump = Input.GetKeyDown(KeyCode.Space);
+        crouch = Input.GetKeyDown(KeyCode.LeftControl);
+        roll = Input.GetKeyDown(KeyCode.LeftAlt);
+    }
+
+    void Move()
+    {
+        if(!isRolling)
+            movement = new Vector3(moveX, 0, moveY);
+        movement = Vector3.ClampMagnitude(movement, 1);
+        movement *= speed * Time.deltaTime;
+
+        if(!isJumping)
+            movement = Camera.main.transform.TransformDirection(movement);
+        else
+            movement = transform.TransformDirection(movement);
+
+        if(!isJumping || !isRolling)
+            rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        if (isCrouching)
+            sprint = false;
+
+        if (movement == Vector3.zero && speed > 0)
+            speed -= runToIdleSpeed * Time.deltaTime;
+        else if (!sprint && speed < runSpeed)
+            speed += idleToRunSpeed * 2 * Time.deltaTime;
+        else if (!isCrouching && sprint && speed < sprintSpeed)
+            speed += runToSprintSpeed * Time.deltaTime;
+        else if (!sprint && speed > runSpeed)
+            speed -= sprintToRunSpeed * Time.deltaTime;
+
+        animator.SetFloat("Speed", speed);
+        animator.SetFloat("AirVelocity", rb.velocity.y);
+
+        if (movement == Vector3.zero || isJumping || isRolling) return;
+        direction = Quaternion.LookRotation(new Vector3(movement.x, 0, movement.z));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, direction, rotationSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (isCrouching || isRolling) return;
+        if (!isJumping && rb.velocity.y > 5 || !isJumping && rb.velocity.y < -5)
+        {
+            isJumping = true;
+            animator.SetBool("Jumping", true);
+            animator.SetBool("Movement", false);
+        }
+
+        if(jump && !isJumping)
+        {
+            isJumping = true;
+            animator.SetBool("Jumping", true);
+            animator.SetBool("Movement", false);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
+        if (isJumping && IsGrounded())
+        {
+            animator.SetBool("Jumping", false);
+            animator.SetBool("Movement", true);
+            isJumping = false;
+        }
+    }
+
+    void Crouch()
+    {
+        if (isJumping || isRolling) return;
+
+        if (crouch)
+            isCrouching = !isCrouching;
+
+        if (isCrouching)
+        {
+            animator.SetBool("Crouching", true);
+            runSpeed = crouchSpeed;
+            sprintSpeed = crouchSpeed;
+            crouchCollider.height = Mathf.Lerp(crouchCollider.height, colliderHeight / 2, resizeSpeed * Time.deltaTime);
+            crouchCollider.center = Vector3.Lerp(crouchCollider.center, new Vector3(0, .4375f, 0), resizeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("Crouching", false);
+            runSpeed = baseRunSpeed;
+            sprintSpeed = baseSprintSpeed;
+            crouchCollider.height = Mathf.Lerp(crouchCollider.height, colliderHeight, resizeSpeed * Time.deltaTime);
+            crouchCollider.center = Vector3.Lerp(crouchCollider.center, new Vector3(0, .875f, 0), resizeSpeed * Time.deltaTime);
+        }
+    }
+
+    void Roll()
+    {
+        if (isJumping || isCrouching) return;
+
+        if(roll && !isRolling)
+        {
+            isRolling = true;
+            animator.SetBool("Rolling", true);
+            rollStartTime = Time.time;
+            rollDirection = Quaternion.LookRotation(new Vector3(movement.x, 0, movement.z));
+        }
+
+        if(isRolling)
+        {
+            rb.velocity = new Vector3(movement.x * rollForce, rb.velocity.y, movement.z * rollForce);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rollDirection, rotationSpeed * Time.deltaTime);
+
+            if (Time.time - rollStartTime >= rollLength)
+            {
+                isRolling = false;
+                animator.SetBool("Rolling", false);
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distanceToGround, groundLayer);
+    }
+}
